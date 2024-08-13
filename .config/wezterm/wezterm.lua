73799b9a598d6131ae7d6a6f294a281b5f59f963
Wataru Utsumi <67275879+watsumi@users.noreply.github.com>
2024-08-13 21:41:44 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,11 +7,11 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- colors
-config.color_scheme = 'Spacedust (Gogh)'
+config.color_scheme = 'Sea Shells (Gogh)'
 -- font
 config.font_size = 13.0
 -- Disable tab bar
