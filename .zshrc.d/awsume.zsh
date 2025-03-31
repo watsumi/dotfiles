6356bb185b23e9382d2629ef2b0e8307c9a62056
watsumi <w.utsumi2@gmail.com>
2025-03-31 22:00:25 +0900

    update

---
+++ .zshrc.d/awsume.zsh
@@ -0,0 +1,1 @@
+alias awsume='source awsume'
---
+++ .zshrc.d/pnpm.zsh
@@ -0,0 +1,5 @@
+export PNPM_HOME="/Users/macadmin1/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
